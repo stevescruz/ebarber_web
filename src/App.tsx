dc2c18c4538bd1d78b5b4501af9dc207bacb052f
stevescruz <steve.silverio.cruz@gmail.com>
2020-07-02 23:52:55 -0400

    Styled Components & Global Style
    -Global Styles CREATED

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Avengers Assemble</h1>;
+import GlobalStyle from './styles/global';
+
+import SignIn from './pages/Signin';
+
+const App: React.FC = () => (
+	<>
+		<SignIn />
+		<GlobalStyle />
+	</>
+);
 
 export default App;
