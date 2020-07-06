fd5535db48f702526b0e82175550c2782d72c86b
stevescruz <steve.silverio.cruz@gmail.com>
2020-07-06 15:26:08 -0400

    Input as an Uncontrolled Component for Sign Up Page
    @unform/core dependency ADDED
    @unform/web dependency ADDED

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ import { Container } from './styles';
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
-	<Container {...rest} type="button">
+	<Container type="button" {...rest}>
 		{children}
 	</Container>
 );
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,36 +1,44 @@
 import React from 'react';
 
 import { FiUser, FiMail, FiLock, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
 import { Container, BackgroundContainer, ContentWrapper } from './styles';
+
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input/index';
 import Button from '../../components/Button/index';
 
-const SignUp: React.FC = () => (
-	<Container>
-		<BackgroundContainer />
+const SignUp: React.FC = () => {
+	function handleSubmit(data: object): void {
+		console.log(data);
+	}
 
-		<ContentWrapper>
-			<img src={logoImg} alt="eBarber Logo" />
-			<form>
-				<h1>Create your eBarber account now</h1>
+	return (
+		<Container>
+			<BackgroundContainer />
 
-				<Input name="name" icon={FiUser} placeholder="Name" />
-				<Input name="email" icon={FiMail} placeholder="E-mail" />
-				<Input
-					name="password"
-					icon={FiLock}
-					type="password"
-					placeholder="Password"
-				/>
-				<Button>Sign up</Button>
-			</form>
-			<a href="/">
-				<FiArrowLeft />
-				Return to sign in
-			</a>
-		</ContentWrapper>
-	</Container>
-);
+			<ContentWrapper>
+				<img src={logoImg} alt="eBarber Logo" />
+				<Form onSubmit={handleSubmit}>
+					<h1>Create your eBarber account now</h1>
 
+					<Input name="name" icon={FiUser} placeholder="Name" />
+					<Input name="email" icon={FiMail} placeholder="E-mail" />
+					<Input
+						name="password"
+						icon={FiLock}
+						type="password"
+						placeholder="Password"
+					/>
+					<Button type="submit">Sign up</Button>
+				</Form>
+				<a href="/">
+					<FiArrowLeft />
+					Return to sign in
+				</a>
+			</ContentWrapper>
+		</Container>
+	);
+};
 export default SignUp;
