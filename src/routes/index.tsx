e152863c3a80465b245ab603aeb88e78bfe147d2
stevescruz <steve.silverio.cruz@gmail.com>
2020-07-25 20:55:18 -0400

    Route Authentication
    -Also created placeholder component for Dashboard.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+import Route from './Route';
 
 const Routes: React.FC = () => {
 	return (
 		<Switch>
 			<Route exact path="/" component={SignIn} />
 			<Route exact path="/signup" component={SignUp} />
+
+			<Route exact path="/dashboard" component={Dashboard} isPrivate />
 		</Switch>
 	);
 };
